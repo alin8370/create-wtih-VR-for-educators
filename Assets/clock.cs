e068f1489a23a926fa179a431ff5887b636bd578
Ana Lin <alin8370@yahoo.com.au>
2022-11-14 21:43:08 -0300

    tried to get the clock to move hinge joint experiment

--- Assets/clock.cs
+++ Assets/clock.cs
@@ -1,10 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class clock : MonoBehaviour
 {
-  
     // Start is called before the first frame update
     private const float
         hoursToDegrees = 360f / 12f,
@@ -12,25 +12,24 @@ public class clock : MonoBehaviour
         secondsToDegrees = 360f / 60f;
 
     public Transform hours, minutes, seconds;
-    private System.DateTime oldTime;
+    private DateTime oldTime;
 
     // Start is called before the first frame update
     void Start()
     {
-        oldTime = System.DateTime.Now;
+        oldTime = DateTime.Now;
     }
 
     // Update is called once per frame
     void Update()
     {
-        System.DateTime time = System.DateTime.Now;
+        DateTime time = DateTime.Now;
         if (time > oldTime)
         {
             seconds.localRotation = Quaternion.Euler(time.Second * secondsToDegrees, 0f, 0f);
 
-            System.TimeSpan timespan = System.DateTime.Now.TimeOfDay;
-         
-    hours.localRotation =
+            TimeSpan timespan = DateTime.Now.TimeOfDay;
+            hours.localRotation =
                 Quaternion.Euler((float)timespan.TotalHours * hoursToDegrees, 0f, 0f);
             minutes.localRotation =
                 Quaternion.Euler((float)timespan.TotalMinutes * minutesToDegrees, 0f, 0f);
@@ -38,6 +37,3 @@ public class clock : MonoBehaviour
         }
     }
 }
-
-
-
